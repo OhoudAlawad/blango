a1b518c6180a463e8631eb5adad800c4adb01247
Ohoud Alawad <ohoudalawad.contact@gmail.com>
2024-08-25 10:47:30 +0000

    Finish django configurations

--- blog/models.py
+++ blog/models.py
@@ -1,18 +1,21 @@
 from django.db import models
 from django.conf import settings
+
+# other imports ommited
 from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes.fields import GenericRelation
-# other imports ommited
 
+# Create your models here.
 class Comment(models.Model):
     creator = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     content = models.TextField()
     content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
     object_id = models.PositiveIntegerField()
     content_object = GenericForeignKey("content_type", "object_id")
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
 
-# Create your models here.
 class Tag(models.Model):
     value = models.TextField(max_length=100)
 
@@ -36,12 +39,4 @@ class Post(models.Model):
 
 
 
-class Comment(models.Model):
-    creator = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    content = models.TextField()
-    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
-    object_id = models.PositiveIntegerField()
-    content_object = GenericForeignKey("content_type", "object_id")
-    created_at = models.DateTimeField(auto_now_add=True)
-    modified_at = models.DateTimeField(auto_now=True)
 
