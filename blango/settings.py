6d45bcb0d87061502019dedd1f2aa8cc2d6be090
Ohoud Alawad <ohoudalawad.contact@gmail.com>
2024-08-27 09:35:43 +0000

    Finish caching

--- blango/settings.py
+++ blango/settings.py
@@ -120,11 +120,11 @@ class Dev(Configuration):
         },
     ]
 
-      PASSWORD_HASHERS = [
-        'django.contrib.auth.hashers.Argon2PasswordHasher',
-        'django.contrib.auth.hashers.PBKDF2PasswordHasher',
-        'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
-        'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
+    PASSWORD_HASHERS = [
+      'django.contrib.auth.hashers.Argon2PasswordHasher',
+      'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+      'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
+      'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
     ]
 
     LOGGING = {
