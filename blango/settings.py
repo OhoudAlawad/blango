fb710dcbd547371134d69ce463f755f83f17d4ed
Ohoud Alawad <ohoudalawad.contact@gmail.com>
2024-08-28 15:29:09 +0000

    Finish database optimization

--- blango/settings.py
+++ blango/settings.py
@@ -54,9 +54,11 @@ class Dev(Configuration):
         "blog",
         "crispy_forms",
         "crispy_bootstrap5",
+        "debug_toolbar",
     ]
 
     MIDDLEWARE = [
+        "debug_toolbar.middleware.DebugToolbarMiddleware",
         'django.middleware.security.SecurityMiddleware',
         'django.contrib.sessions.middleware.SessionMiddleware',
         'django.middleware.common.CommonMiddleware',
@@ -193,6 +195,7 @@ class Dev(Configuration):
     # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
     DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+    INTERNAL_IPS = ["192.168.11.179"]
 
 
 class Prod(Dev):
