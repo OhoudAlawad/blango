d0bf7b5bb5b7dffb59ae61bfcfbc73f7a435bf4c
Ohoud Alawad <ohoudalawad@gmail.com>
2026-06-19 14:31:10 +0300

    Suppress bandit hardcoded secret warning for development setting

--- blango/settings.py
+++ blango/settings.py
@@ -26,7 +26,7 @@ class Dev(Configuration):
     # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
     # SECURITY WARNING: keep the secret key used in production secret!
-    SECRET_KEY = 'django-insecure-+sn%dpa!086+g+%44z9*^j^q-u4n!j(#wl)x9a%_1op@zz2+1-'
+    SECRET_KEY = 'django-insecure-+sn%dpa!086+g+%44z9*^j^q-u4n!j(#wl)x9a%_1op@zz2+1-'  # nosec B105
 
     # SECURITY WARNING: don't run with debug turned on in production!
     # SECURE: Debug mode controlled via environment variables
